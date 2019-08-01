f6f3c6c0db579a0e123b1e88db04ae99fa66f370
Pitter <snider@degiant.com.br>
2019-08-01 01:09:16 -0300

    :sparkles: Add first files

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const http = require('http');
+const socketIo = require('socket.io');
+const onChange = require('on-change');
+
+/**
+ * Send the proxified object to one (or more) socket(s).
+ */
+function emitUpdate(socket, proxifiedObject) {
+  socket.emit('update', proxifiedObject);
+}
+
+/**
+ * Open a socket.io server and send updates to connected clients
+ * when a specific object suffer changes in one or more properties.
+ *
+ * @param {number} port The port to open the HTTP socket server
+ * @param {object} initialObject The default value for the object to be proxified
+ */
+function createProxifiedObjectSocket(options = {
+  port = 8181,
+  initialObject = {},
+}) {
+  const server = http.createServer();
+  const io = socketIo(server);
+
+  // Start the socket server
+  server.listen(options.port);
+
+  io.on('connection', socket => emitUpdate(socket, proxifiedObject));
+  return onChange(options.initialObject, () => emitUpdate(io.sockets, proxifiedObject));
+}
+
+module.exports = createProxifiedObjectSocket;
